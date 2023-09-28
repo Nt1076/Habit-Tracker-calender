4df9715231c7803b6d29eaf5cea05411dd55495c
flutter <marvelnishant@001@gmail.com>
2023-09-28 14:31:30 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:habbit_tracker/pages/home.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void main() async {
+  // initialize hive
+  await Hive.initFlutter();
+
+  // open a box
+  await Hive.openBox("Habit_Database");
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.green,),
+      home: HomePage(),
+      
+    );
+  }
+}
+
